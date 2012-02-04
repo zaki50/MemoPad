95405dfbb11f78a98419f3746ec4746c137a2612
mstssk <mstssk+github@gmail.com>
2012-02-04 13:08:00 +0900

    SmartTag用アプリプロジェクト追加

---
+++ smart_tag_app/src/org/zakky/smart_tag_app/SmartTagAppActivity.java
@@ -0,0 +1,13 @@
+package org.zakky.smart_tag_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SmartTagAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
